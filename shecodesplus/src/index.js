4225efa59ffdb17bdc7ee633a702693839efe5e8
kikilani <66570117+kikilani@users.noreply.github.com>
2021-03-08 10:12:48 -0500

    Moved weather app files to repository

---
+++ shecodesplus/src/index.js
@@ -0,0 +1,52 @@
+let now = new Date();
+let currentTime = document.querySelector(".current-time");
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = months[now.getMonth()];
+currentTime.innerHTML = `It is ${hours}:${minutes} on ${day}, ${month} ${date}`;
+
+function getTemperature(response){
+let temperature = Math.round(response.data.main.temp);
+let maxTemp = Math.round(response.data.main.temp_max);
+let minTemp = Math.round(response.data.main.temp_min);
+let currentTemperature = document.querySelector(".current-temp");
+currentTemperature.innerHTML = `${temperature}°F`
+let highTemp = document.querySelector(".high-temp");
+highTemp.innerHTML =`H ${maxTemp}°F |`
+let lowTemp = document.querySelector(".low-temp");
+lowTemp.innerHTML = `L ${minTemp}°F`
+let weather = response.data.weather[0].description;
+let weatherDescription = document.querySelector(".weather-description");
+weatherDescription.innerHTML =`${weather}`;
+
+
+}
+function citySearch(event){
+    event.preventDefault();
+    let cityInput = document.querySelector(".search");
+let currentCity = document.querySelector(".current-city");
+currentCity.innerHTML =`${cityInput.value}`;
+let unit =`imperial`;
+let apiKey=`d8cb484f7bb8a25ccc8f43355441ee5f`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=${unit}&appid=${apiKey}`;
+ axios.get(apiUrl).then(getTemperature);
+}
+let submitSearch = document.querySelector(".city");
+submitSearch.addEventListener("submit", citySearch)
